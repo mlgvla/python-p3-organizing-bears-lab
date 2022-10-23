f3eb960043a81710f56db081789d681b200dbfb9
Monica Gerard <mlgvla@gmail.com>
2022-10-23 19:04:56 -0400

    solution

---
+++ lib/insert.sql
@@ -0,0 +1,33 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Mr. Chocolate", 1, "M", "Brown", "Friendly", TRUE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Rowdy", 1, "M", "Black", "Friendly", TRUE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Tabitha", 2, "F", "Brown", "Mean", TRUE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Sergeant Brown", 1, "M", "Brown", "Friendly", TRUE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Melissa", 1, "F", "Brown", "Friendly", TRUE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Grinch", 1, "M", "Brown", "Friendly", TRUE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Wendy", 1, "F", "Brown", "Angry", TRUE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (NULL, 1, "M", "Brown", "Friendly", TRUE);
+
+
+
+
+
+
+
+
+
+
